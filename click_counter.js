2f0953ca30d905cc6ad38fae195d0aba5867442a
Chetan <43693378+chetangaur@users.noreply.github.com>
2021-10-06 18:00:26 +0530

    Click counter to understand react useState hook

---
+++ click_counter.js
@@ -0,0 +1,24 @@
+
+//import './App.css';
+import React, { useState } from 'react';
+
+//let count=0;
+
+
+
+const App = () =>{
+  //const state = useState();
+  const [count,setCount] = useState(0);           // Destructuring of Array 
+
+  const IncNum = () => {
+    setCount(count+1);
+  };
+  return(
+    <>
+    <h1>{count}</h1>
+    <button onClick={IncNum}>Click Me</button>
+    </>
+  );
+};
+
+export default App;
